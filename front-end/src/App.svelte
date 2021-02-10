f067bf4dcde96bd71993466f052093cd6786ef94
Ovais Oozeer <ovais.oozeer@users.noreply.github.com>
2021-02-10 23:53:16 +0000

    Adds button for testing

--- front-end/src/App.svelte
+++ front-end/src/App.svelte
@@ -1,30 +1,19 @@
-<script lang="ts">
-	export let name: string;
-</script>
+<script>
+	let state = 'Not ready';
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
+	function handleClick() {
+		switch (state)
+		{
+			case 'Not ready':
+				state = 'Ready';
+				break;
+			case 'Ready':
+				state = 'High five!';
+				break;
 		}
 	}
-</style>
+</script>
+
+<button on:click={handleClick}>
+	State: {state}
+</button>
