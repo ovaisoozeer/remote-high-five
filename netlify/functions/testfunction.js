15ed124346f2a702a081e40282f4b0834abaf786
Ovais Oozeer <ovais.oozeer@users.noreply.github.com>
2021-02-20 13:46:33 +0000

    Testing environment variable access from function

--- netlify/functions/testfunction.js
+++ netlify/functions/testfunction.js
@@ -1,5 +1,5 @@
 exports.handler = async event => {
-    const subject = event.queryStringParameters.name || 'World'
+    const subject = event.queryStringParameters.name || process.env.OVAISTEST
     return {
       statusCode: 200,
       body: `Hello ${subject}!`,
